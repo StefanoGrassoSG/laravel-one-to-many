a97ad73ef6e9a19763fb45c46dd1b15b62d52a40
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-21 16:49:20 +0200

    ADD migration project

---
+++ database/migrations/2023_09_21_143809_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+            $table->string('slug');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('project_status', 32);
+            $table->jsonb('languages');
+            $table->string('project_link', 2083);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
