814cbcf994922b1ad3f7a952415997f7e03398f4
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-21 16:35:45 +0200

    ADD basic routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//controllers 
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\Admin\PrivateController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,12 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Public route
+Route::get('/', [PublicController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//private route
+Route::get('admin/dashboard', [PrivateController::class, 'dashboard'])
+    ->middleware(['auth', 'verified'])
+    ->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
